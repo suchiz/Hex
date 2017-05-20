c68d0d247616be523e4b3a14f448c834d103e518
Slyyl31 <Sylvain-31270@hotmail.fr>
2017-05-20 04:59:28 +0200

    Stack(Historique) + Undo (pb : cf Todo)

--- lib/jouer.h
+++ lib/jouer.h
@@ -12,20 +12,50 @@
 /********************
  * ERROR DEFINITION *
  ********************/
-#define OK		0
-#define ERR_CREATE_NODE	1
+#define OK			0
+#define ERR_CREATE_NODE		1
 #define ERR_CREATE_GROUP	2
 #define ERR_CREATE_ELEMENT	3
 #define ERR_CREATE_GRAPH	4
-#define ERR_ADD_EDGE	5
+#define ERR_ADD_EDGE		5
+#define ERR_CREATE_DATA		6
+/**********************
+ * STRUCTURE OF STACK *
+ **********************/
+typedef struct stack_s
 
+{
+  int size;
+ struct element_data_s *first;
+}stack;
+
+/*********************
+ * STRUCTURE OF DATA *
+ *********************/
+typedef struct element_data_s{
+    int turn;
+    int player;
+    int x;
+    int y;
+    struct element_data_s *next;
+
+}element_data;
 
 int coordonnee(const board* b,int x,int y);
 
 int coup_valide(const board *b, int x, int y, int ind);
 
-void jouer_coup(board *b, int *joueur, int *tour, element_group *eg);
+void jouer_coup(board *b,stack* stack, int *joueur, int *tour, element_group *eg);
 
 void startGame();
 
+void createStack (stack* stack);
+
+void push(stack *stack, int turn, int player,int x,int y);
+
+void pop(stack *stack);
+
+void displayStack(stack* stack);
+
+stack* undo(board *b,stack *stack);
 #endif
